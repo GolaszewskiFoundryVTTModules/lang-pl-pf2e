c9b55b60cbad244ecfff12b901ce91fd4639d3d0
Michał Gołaszewski <46850235+MichalGolaszewski@users.noreply.github.com>
2023-11-24 10:30:53 +0100

    Added optional update for source locale

--- tools/HelperScripts/updateLocalization.py
+++ tools/HelperScripts/updateLocalization.py
@@ -4,6 +4,9 @@ import logging
 from tqdm import tqdm
 from collections import Counter
 import datetime
+import shutil
+import subprocess
+import argparse
 
 class LocalizationUpdater:
     
@@ -191,6 +194,40 @@ class LocalizationUpdater:
 
         self.save_file_to_directory(self.pl_path, self.rebuild_nested_json(self.pl_extracted))
 
+def copy_files_and_directories(src_directory, dst_directory):
+    """
+    Recursively copy all files and directories from the source directory 
+    to the destination directory. Existing files in the destination directory 
+    with the same name will be overwritten.
+    """
+    if not os.path.exists(dst_directory):
+        os.makedirs(dst_directory)
+    for item in os.listdir(src_directory):
+        src_path = os.path.join(src_directory, item)
+        dst_path = os.path.join(dst_directory, item)
+        if os.path.isdir(src_path):
+            # Recursively copy the entire subdirectory
+            if os.path.exists(dst_path):
+                # If the subdirectory exists at the destination, recursively copy its contents
+                copy_files_and_directories(src_path, dst_path)
+            else:
+                # If the subdirectory does not exist at the destination, copy it
+                shutil.copytree(src_path, dst_path)
+        else:
+            # Copy files and overwrite if necessary
+            shutil.copy2(src_path, dst_path)
+
+def run_js_script(script_path):
+    try:
+        # Run the .js script with Node.js
+        result = subprocess.run(['node', script_path], check=True, text=True, capture_output=True)
+        # Output the result (stdout and stderr)
+        print(result.stdout)
+        print(result.stderr)
+    except subprocess.CalledProcessError as e:
+        # Handle errors in the called .js script
+        print(f"An error occurred while running {script_path}: {e}")
+
 def main():
     # Setup logging configuration
     log_directory = "tools/HelperScripts/Logs"
@@ -198,6 +235,16 @@ def main():
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     log_filename = os.path.join(log_directory, f"LocalizationUpdate_{current_time}.log")
     logging.basicConfig(level=logging.INFO, filename=log_filename, filemode='w', format='%(message)s')
+
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(description='Run the script with optional update source data flag.')
+    parser.add_argument('--UpdateSourceData', action='store_true', help='Update source data if set to true.')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # The flag is False by default, True if --UpdateSourceData is used in the command line
+    update_source_data = args.UpdateSourceData
 
     # Define sets of file paths
     file_sets = []
@@ -256,6 +303,13 @@ def main():
             )
         )
 
+    if(update_source_data):
+        # back up the old localization source
+        copy_files_and_directories(core_en_directory, core_en_old_directory)
+
+        # update the source files
+        print("Running the pack extractor...")
+        run_js_script("src/pack-extractor/pack-extractor.js")
 
     for en_old, en, pl in file_sets:
         updater = LocalizationUpdater(en_old, en, pl)

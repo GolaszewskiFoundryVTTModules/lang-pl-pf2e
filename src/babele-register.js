b51288345cd82a10de8f5040373e31329e0ad262
Michał Gołaszewski <golaszewski.michal.97@gmail.com>
2024-03-16 19:06:15 +0100

    Added auto-translate for automated animations

--- src/babele-register.js
+++ src/babele-register.js
@@ -124,3 +124,63 @@ Hooks.once("i18nInit", () => {
         removeMismatchingTypes(fallback, game.i18n.translations);
     }
 });
+
+Hooks.once("ready", () => {
+    // Register auto-translation of Automated Animations
+    if (!game.modules.has("autoanimations")) {
+        return;
+    }
+    
+    Hooks.on("AutomatedAnimations-WorkflowStart", (data, animationData) => {
+        if (animationData) return;
+
+	let changed = false;
+
+	    console.log(data);
+	    console.log(data.item?.flags?.babele?.originalName);
+	    console.log(data.ammoItem?.flags?.babele?.originalName);
+	    console.log(data.originalItem?.flags?.babele?.originalName);
+	    console.log(animationData);
+
+        if (data.item?.flags?.babele?.originalName) {
+            data.item = createItemNameProxy(
+                data.item,
+                data.item.flags.babele.originalName
+            );
+            changed = true;
+        }
+	
+        if (data.ammoItem?.flags?.babele?.originalName) {
+            data.ammoItem = createItemNameProxy(
+                data.ammoItem,
+                data.ammoItem.flags.babele.originalName
+            );
+            changed = true;
+        }
+	
+        if (data.originalItem?.flags?.babele?.originalName) {
+            data.originalItem = createItemNameProxy(
+                data.originalItem,
+                data.originalItem.flags.babele.originalName
+            );
+            changed = true;
+        }
+
+        if (changed) {
+            data.recheckAnimation = true;
+        }
+
+	console.log(data);
+    });
+});
+
+function createItemNameProxy(item, realName) {
+    return new Proxy(item, {
+        get(target, p, receiver) {
+        if (p === "name") {
+            return realName;
+        }
+        return Reflect.get(target, p, receiver);
+        },
+    });
+}

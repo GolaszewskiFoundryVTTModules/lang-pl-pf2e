fab8909d34be37e1cba6490a421b4219ef2f7107
Michał Gołaszewski <golaszewski.michal.97@gmail.com>
2024-07-26 09:20:30 +0200

    c.d.

--- tools/LocalizationUpdater/update_localization.py
+++ tools/LocalizationUpdater/update_localization.py
@@ -8,25 +8,37 @@ from localization_updater import LocalizationUpdater
 from translator_config import log_directory, log_filename, temp_en_old_directory, core_en_directory, core_pl_directory, en_to_pl_file_pairs
 
 def copy_files_and_directories(src_directory, dst_directory):
+    """
+    Recursively copy all files and directories from the source directory 
+    to the destination directory. Existing files in the destination directory 
+    with the same name will be overwritten.
+    """
     if not os.path.exists(dst_directory):
         os.makedirs(dst_directory)
     for item in os.listdir(src_directory):
         src_path = os.path.join(src_directory, item)
         dst_path = os.path.join(dst_directory, item)
         if os.path.isdir(src_path):
+            # Recursively copy the entire subdirectory
             if os.path.exists(dst_path):
+                # If the subdirectory exists at the destination, recursively copy its contents
                 copy_files_and_directories(src_path, dst_path)
             else:
+                # If the subdirectory does not exist at the destination, copy it
                 shutil.copytree(src_path, dst_path)
         else:
+            # Copy files and overwrite if necessary
             shutil.copy2(src_path, dst_path)
 
 def run_js_script(script_path):
     try:
+        # Run the .js script with Node.js
         result = subprocess.run(['node', script_path], check=True, text=True, capture_output=True)
+        # Output the result (stdout and stderr)
         print(result.stdout)
         print(result.stderr)
     except subprocess.CalledProcessError as e:
+        # Handle errors in the called .js script
         print(f"An error occurred while running {script_path}: {e}")
 
 def main():

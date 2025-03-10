2243e42930e934e1a58695617214191d6da040ec
Michał Gołaszewski <golaszewski.michal.97@gmail.com>
2025-03-10 17:46:40 +0100

    6.10.2 update

--- tools/LocalizationUpdater/translator_config.py
+++ tools/LocalizationUpdater/translator_config.py
@@ -106,4 +106,5 @@ en_to_pl_file_pairs = [
     ("compendium/triumph-of-the-tusk-bestiary", "compendium/pf2e.triumph-of-the-tusk-bestiary"),
     ("compendium/war-of-immortals-bestiary", "compendium/pf2e.war-of-immortals-bestiary"),
     ("compendium/spore-war-bestiary", "compendium/pf2e.spore-war-bestiary"),
+    ("compendium/pathfinder-npc-core", "compendium/pf2e.pathfinder-npc-core"),
 ]
